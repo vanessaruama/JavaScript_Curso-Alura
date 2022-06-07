0905e3df411d213bfe7b9cba2bd2594a8670c047
Vanessa Ruama <52838836+vanessaruama@users.noreply.github.com>
2022-06-07 09:24:22 -0300

    Commits do curso de JavaScript do Alura
    Commits do curso de JavaScript do Alura

---
+++ js/calcula-imc.js
@@ -0,0 +1,69 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++){
+
+    var paciente = pacientes[i]
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var pesoValido = validaPeso(peso);
+    var alturaValido = validaAltura(altura);
+
+    if (!pesoValido) {
+        console.log("Peso inválido")
+        pesoValido = false;
+        tdImc.textContent = "Peso Inválido"
+        paciente.classList.add(paciente-invalido);
+    }
+    
+    if (!alturaValido){
+        console.log("Altura inválido")
+        alturaValido = false;
+        tdImc.textContent = "Altura Inválido"
+        paciente.classList.add(paciente-invalido);
+    }
+
+    if (pesoValido && alturaValido){
+
+        var tdImc = paciente.querySelector(".info-imc");
+        var imc = calculaImc(peso, altura);
+        tdImc.textContent = imc;
+    }
+}
+
+function validaPeso(peso){
+    if(peso >= 0 && peso < 1000){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validaAltura(altura){
+    if(altura >= 0 && peso <= 3.0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function calculaImc(peso, altura){
+    var imc = 0;
+
+    imc = peso / (altura * altura); // 100 / 2.0 * 2.0 = 100 / 4 =>>> 25
+
+    return imc.toFixed(2)
+}
+
+function validaPaciente(paciente){
+    if(paciente.altura <3.0 && paciente.altura >= 0){
+        return true;
+    }
+}
